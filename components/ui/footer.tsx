c4b7bad3e122373140e63fbf2307b3bcb06dc47f
Hrvoje Kasabasic <hrvoje.kasabasic@mashpipe.io>
2023-05-30 13:04:45 +0200

    footer divider

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,6 +1,10 @@
+import { HeroBackground } from "@/components/home/HeroBackground";
+
 export default function Footer() {
   return (
-    <footer>
+    <footer className="relative">
+      <HeroBackground secondary className="h-32 -top-32 overflow-hidden" />
+
       <div className="max-w-4xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-16 border-t border-gray-200">
           <div className="md:flex md:items-center md:justify-center">
