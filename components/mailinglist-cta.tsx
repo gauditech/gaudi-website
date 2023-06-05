88c83a66a7d20012cf348b9d02854a60cc16c361
Marin Pranjić <marin@mpranjic.com>
2023-06-05 16:58:10 +0200

    Make the subscribe feature work

--- components/mailinglist-cta.tsx
+++ components/mailinglist-cta.tsx
@@ -5,17 +5,25 @@ export default function MailinglistCta() {
         <div className="text-xl font-medium mb-6">
           Stay in touch! Don't miss Gaudi news and updates
         </div>
-        <form>
+        <form action="https://api.web3forms.com/submit" method="POST">
+          <input type="hidden" name="access_key" value="7ec941be-5bb0-4f30-88d9-654c7ee5fc6d" />
+
+          <input type="hidden" name="name" value="New Subscriber" />
+          <input type="hidden" name="message" value="New Subscription on gaudi.tech" />
+          <input type="hidden" name="redirect" value="https://web3forms.com/success" />
+          <input type="checkbox" name="botcheck" className="hidden" style={{display: "none"}} />
+
           <div className="relative flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-sm sm:px-4">
             <input
               type="email"
-              className="form-input w-full px-4 py-2 mb-2 sm:mb-0 sm:mr-2"
+              name="email"
+              className="form-input w-full px-4 py-2 mb-2 sm:mb-0 sm:mr-2 text-black"
               placeholder="Your email"
               aria-label="Your email"
             />
-            <a className="btn-sm btn-blue" href="#0">
+            <button className="btn-sm btn-blue">
               Subscribe
-            </a>
+            </button>
             <svg
               className="hidden sm:block absolute left-full -mt-3"
               width="42"
